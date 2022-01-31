0a4a8adfbf1eeac3bdced4ec8d37b5842e075296
yazninja <yazlesean@gmail.com>
2022-02-01 00:03:35 +0800

    Make public folder work

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
 app.listen(port, function(){
     console.log("Listening on port " + port);
 });
-app.use(express.static(__dirname =  "/public"));
+app.use(express.static(__dirname =  "./public"));
 
 app.set("view engine", "hbs");
 app.engine("hbs", engine({extname: "hbs"}));
