10565e8e5929a77c761162441b5520394b69aba9
yazninja <yazlesean@gmail.com>
2022-02-01 15:27:58 +0800

    Add logic for User in database

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const express = require("express");
+const app = express();
 const { engine } = require("express-handlebars");
 const res = require("express/lib/response");
-
-const mongoose = reuire('mongoose');
-
+// Mongoose
+const mongoose = require('mongoose');
+const User = require("../database/models/User");
 const dbURL = 'mongodb://localhost:27017/pesotrack';
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
 
 mongoose.connect(dbURL, options, function(){
-    console.log("Connected to DB");
+    console.log("Connected to DB @ " + dbURL);
 });
 
-const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const port = 3000;
-app.listen(port, function(){
-    console.log("Listening on port " + port);
-});
+
 app.use(express.static(__dirname =  "./public"));
 
 app.set("view engine", "hbs");
@@ -24,4 +23,16 @@ app.engine("hbs", engine({extname: "hbs"}));
 
 app.get("/", function(req, res){
     res.render("index");
+});
+
+app.post("/register", function(req, res){
+    console.log(req.body);
+    User.create(req.body, (error, post) => {
+        res.redirect("/dashboard");
+    });
+});
+
+const port = 3000;
+app.listen(port, function(){
+    console.log("Listening on port " + port);
 });
