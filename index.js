1328859e8fac1bcd30baf58673d5eb05f38f2d33
yazninja <yazlesean@gmail.com>
2022-02-06 00:25:38 +0800

    Create Session to use node-login-starter

--- index.js
+++ index.js
@@ -3,40 +3,31 @@ const app = express();
 const Handlebars = require('handlebars');
 const { engine } = require("express-handlebars");
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
-const res = require("express/lib/response");
-// Mongoose
-const mongoose = require('mongoose');
-const dbURL = 'mongodb://localhost:27017/pesotrack';
-const options = { useNewUrlParser: true, useUnifiedTopology: true };
-mongoose.connect(dbURL, options, function () {
-    console.log("Connected to DB @ " + dbURL);
-});
 const appRouter = require('./routes/appRoutes');
-// const session = require('express-session');
-// const flash = require('connect-flash');
-// const MongoStore = require('connect-mongo')(session);
-// var user = new User();
+const session = require('express-session');
+const flash = require('connect-flash');
+const MongoStore = require('connect-mongo');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname = "./public"));
 // Session  
-// app.use(session({
-//     secret: 'somegibberishsecret',
-//     store: new MongoStore({ mongooseConnection: mongoose.connection }),
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24 * 7 }
-// }));
+app.use(session({
+    secret: '47594fcfb4557e880d9158b01fa75e2b1c1dfe725da5000001fabc472244de2c', // secret key= SHA256(pesotrack)
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost:27017/pesotrack' }),
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24 * 7 }
+}));
 // Flash
-// app.use(flash());
+app.use(flash());
 
 // Global messages vars
-// app.use((req, res, next) => {
-//   res.locals.success_msg = req.flash('success_msg');
-//   res.locals.error_msg = req.flash('error_msg');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  next();
+});
 
 app.set("view engine", "hbs");
 app.engine("hbs", engine({ extname: "hbs", handlebars: allowInsecurePrototypeAccess(Handlebars) }));
