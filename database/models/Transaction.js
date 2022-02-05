07435ea4b276cf0765507d0c93d56e13bc7f1eb9
yazninja <yazlesean@gmail.com>
2022-02-05 17:55:19 +0800

    Add page creates transactions

--- database/models/Transaction.js
+++ database/models/Transaction.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const TransactionSchema = new mongoose.Schema({
+    title: String, // Title of the transaction
     username: String, // username of the user who made the transaction
     type: String, // income or expense
     amount: Number, // amount of the transaction
@@ -8,7 +9,7 @@ const TransactionSchema = new mongoose.Schema({
     description: String, // description of the transaction
     category: String, // category of the transaction
     recurring: Boolean, // whether the transaction is recurring
-    recurringType: String, // type of recurring transaction
+    recurringType: Number, // type of recurring transaction
     recurringEndDate: String // end date of recurring transaction
 })
 
--- public/js/addpage.js
+++ public/js/addpage.js
@@ -1,23 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.add_expense').hide();
     $('label[for=custom_category]').hide();
     $('.custom_category').hide();
-    $(".iPresets").change(function(){
+    $(".iPresets").change(function () {
         let today = new Date().toISOString().substr(0, 10);
         $("label[for=sdate]").text("Start Date:");
-        if($('.iPresets').val() == "mSalary"){
+        if ($('.iPresets').val() == "mSalary") {
             $(".repeat").val("3");
             $('.sDate').val(today);
             $('.eDate').fadeIn();
             $('label[for=edate]').fadeIn();
         }
-        else if($('.iPresets').val() == "aSalary"){
+        else if ($('.iPresets').val() == "aSalary") {
             $(".repeat").val("4");
             $('.sDate').val(today);
             $('.eDate').fadeIn();
             $('label[for=edate]').fadeIn();
         }
-        else if($('.iPresets').val() == "dSalary"){
+        else if ($('.iPresets').val() == "dSalary") {
             $(".repeat").val("0");
             $('.sDate').val(today);
             $('.eDate').fadeOut();
@@ -25,36 +25,44 @@ $(document).ready(function(){
             $("label[for=sdate]").text("Date:");
         }
     });
-    $('.repeat').change(function(){
+    $('.repeat').change(function () {
         $('.iPresets').val("custom");
-        $('.ePresets').val("custom");
-        if($('.repeat').val() == "0"){
+        if ($('.repeat').val() == "0") {
             $('.eDate').fadeOut();
-            $('label[for=edate]').fadeOut();
-            $("label[for=sdate]").text("Date:");
+            $('.lbl_sDate').text("Date");
         }
-        else{
+        else {
             $('.eDate').fadeIn();
-            $('label[for=edate]').fadeIn();
-            $("label[for=sdate]").text("Start Date:");
+            $('.lbl_sDate').text("Start Date");
         }
     });
-    $('.ePresets').change(function(){
+    $('.erepeat').change(function () {
+        $('.ePresets').val("custom");
+        if ($('.erepeat').val() == "0") {
+            $('.eeDate').fadeOut();
+            $('.elbl_sDate').text("Date");
+        }
+        else {
+            $('.eeDate').fadeIn();
+            $('.elbl_sDate').text("Start Date");
+        }
+    });
+    $('.ePresets').change(function () {
         let today = new Date().toISOString().substr(0, 10);
         $("label[for=sdate]").text("Start Date:");
-        if($('.ePresets').val() == "mExpense"){
+        if ($('.ePresets').val() == "mExpense") {
             $(".repeat").val("3");
             $('.sDate').val(today);
             $('.eDate').fadeIn();
             $('label[for=edate]').fadeIn();
         }
-        else if($('.ePresets').val() == "aExpense"){
+        else if ($('.ePresets').val() == "aExpense") {
             $(".repeat").val("4");
             $('.sDate').val(today);
             $('.eDate').fadeIn();
             $('label[for=edate]').fadeIn();
         }
-        else if($('.ePresets').val() == "dExpense"){
+        else if ($('.ePresets').val() == "dExpense") {
             $(".repeat").val("0");
             $('.sDate').val(today);
             $('.eDate').fadeOut();
@@ -62,31 +70,33 @@ $(document).ready(function(){
             $("label[for=sdate]").text("Date:");
         }
     });
-    $('.category').change(function(){
-    if($('.category').val() == "custom"){
-        $('label[for=custom_category]').fadeIn();
-        $('.custom_category').fadeIn();
-    }
-    else{
-        $('label[for=custom_category]').fadeOut();
-        $('.custom_category').fadeOut();
-    }
+    $('.category').change(function () {
+        if ($('.category').val() == "Other") {
+            $('label[for=custom_category]').fadeIn();
+            $('.custom_category').fadeIn();
+        }
+        else {
+            $('label[for=custom_category]').fadeOut();
+            $('.custom_category').fadeOut();
+        }
     });
-    $('.inc').click(function(){
+    $('.inc').click(function () {
         $('.inc').addClass('activetab');
         $('.exp').removeClass('activetab');
         $('.add_expense').hide();
         $('label[for=custom_category]').hide();
         $('.custom_category').hide();
         $('.add_income').show();
+        
     });
-    $('.exp').click(function(){
+    $('.exp').click(function () {
         $('.exp').addClass('activetab');
         $('.inc').removeClass('activetab');
         $('.add_income').hide();
         $('.add_expense').show();
         $('label[for=custom_category]').show();
-        $('.custom_category').show();
+        $('.custom_category').hide();
     });
+    
 
 });
